2bee004a6047ffcc72a4c68f64d490593a8531ee
Sonia <soniaccodes@gmail.com>
2026-07-09 22:35:39 -0400

    Add 3 unit tests for ml/model.py functions; add unit_test.png screenshot

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,42 @@
-import pytest
-# TODO: add necessary import
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+from ml.model import compute_model_metrics, inference, train_model
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_train_model_returns_random_forest():
     """
-    # add description for the second test
+    Test that the train_model function returns a RandomForestClassifier
+    instance.
     """
-    # Your code here
-    pass
+    X_train = np.array([[0, 0], [1, 1], [2, 2], [3, 3]])
+    y = np.array([0, 1, 0, 1])
+    model = train_model(X_train, y)
+    assert isinstance(model, RandomForestClassifier)
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_inference_returns_expected_shape():
     """
-    # add description for the third test
+    Test that the inference function returns a numpy array of predictions
+    with one prediction per input row.
     """
-    # Your code here
-    pass
+    X_train = np.array([[0, 0], [1, 1], [2, 2], [3, 3]])
+    y_train = np.array([0, 1, 0, 1])
+    model = train_model(X_train, y_train)
+    X_test = np.array([[0, 0], [1, 1], [2, 2]])
+    preds = inference(model, X_test)
+    assert isinstance(preds, np.ndarray)
+    assert preds.shape[0] == X_test.shape[0]
+
+
+def test_compute_model_metrics_perfect_predictions():
+    """
+    Test that compute_model_metrics returns precision, recall, and fbeta
+    of 1.0 for perfect predictions.
+    """
+    y = np.array([0, 1, 1, 0, 1])
+    pred = np.array([0, 1, 1, 0, 1])
+    precision, recall, fbeta = compute_model_metrics(y, pred)
+    assert precision == 1.0
+    assert recall == 1.0
+    assert fbeta == 1.0
